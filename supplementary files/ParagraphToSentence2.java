8e10fb20b6704ffa3654ff77eabb761944200ec6
our-work-list <132989134+phd-work-22@users.noreply.github.com>
2023-10-28 20:20:18 +0200

    Update ParagraphToSentence2.java

--- supplementary files/ParagraphToSentence2.java
+++ supplementary files/ParagraphToSentence2.java
@@ -1,11 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-/**
- *
- * @author p306654
- */
+
 package paragraphtosentence;
 import java.io.BufferedReader;
 import java.io.File;
