ff73b0b7b3de211864690aaeb444f1af4a69a340
our-work-list <132989134+phd-work-22@users.noreply.github.com>
2024-04-16 13:27:16 +0200

    Update ParagraphToSentence2.java

--- supplementary files/ParagraphToSentence2.java
+++ supplementary files/ParagraphToSentence2.java
@@ -17,21 +17,19 @@ public class ParagraphToSentence2 {
         // Read the files
         // File path is passed as parameter
         String[] params = new String [2];
-        params[0] = "/home/p306654/Documents/SEfiles/sentistrength.sh";
-        params[1] = "/home/p306654/Documents/SEfiles/gentoo_mlists_normalised_messages.txt";
+        params[0] = "sentistrength.sh";
+        params[1] = "filename_of_messages";
         Runtime.getRuntime().exec(params);
         
         File file = new File (params[1]);
  
-        // Creating an object of BufferedReader class
         BufferedReader br
             = new BufferedReader(new FileReader(file));
  
         // Declaring a string variable
         String comments;
        
-        FileWriter fileWriter = new FileWriter
-        ("/home/p306654/Documents/SEfiles/editedSample.txt");
+        FileWriter fileWriter = new FileWriter(filename);
         try (PrintWriter printWriter = new PrintWriter(fileWriter)) {
             int id=1;
             while ((comments = br.readLine()) != null) {
